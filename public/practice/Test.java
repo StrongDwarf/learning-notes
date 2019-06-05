cc8101cd175dc1ac543fec5bcd0ebb7a63f1bd97
xiaobaicai <1908654110@qq.com>
2019-06-05 21:24:25 +0800

    complete 继承.md

--- public/practice/Test.java
+++ public/practice/Test.java
@@ -1,11 +1,25 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class Test{
-    public static void main(String[] args){
-        int[][] magicSquare = {
-            {1,2,3,4},
-            {5,6,7,8}
+import com.sun.glass.ui.Size;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Size.SMALL.toString());
+    }
+
+    enum Size {
+        SMALL("S"), MEDIUM("M"), LARGE("L"), EXTRA_LARGE("XL");
+
+        private String abbreviation;
+
+        private Size(String abbreviation) {
+            this.abbreviation = abbreviation;
         }
-        System.out.println(magicSquare[1][1] + "");
+
+        public String getAbbreviation() {
+            return abbreviation;
+        }
     }
 }
